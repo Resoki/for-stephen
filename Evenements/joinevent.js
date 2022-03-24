4f3237a8cb95445f696ed0449dcefd5e1b3b19f5
Resoki <remyaubert71@gmail.com>
2022-03-24 16:50:40 +0100

    test

--- Evenements/joinevent.js
+++ Evenements/joinevent.js
@@ -12,10 +12,7 @@ module.exports = {
         const test = () => {
             if(member.roles.cache.has(global.roleKick)){
                 const channel = member.guild.channels.cache.find(channel => channel.id === global.channelJoin);
-                member.kick().catch(()=>{
-                    const errorKick = new MessageEmbed().setDescription(`Une erreur a eu lieu en tentant de kick ${member.user.username} ! ❌`).setColor('RED');
-                    channel.send({embeds: [errorKick]})
-                })
+                member.kick()
                 console.log(`${member.user.username} a été kick au bout des 10 minutes! ✅`)
                
                 const successKick = new MessageEmbed().setDescription(`${member.user.username} a été kick au bout des 10 minutes ! ✅`).setColor('GREEN');
@@ -23,11 +20,14 @@ module.exports = {
               
             }
         setTimeout(()=> test(), 6600) // 10 minutes > 600.000 millisecondes
+        }
   
     } 
     catch(err) {
         //channel.send('Une erreur a eu lieu: ', err);
         console.log(err)
+        const errorKick = new MessageEmbed().setDescription(`Une erreur a eu lieu en tentant de kick ${member.user.username} ! ❌`).setColor('RED');
+        channel.send({embeds: [errorKick]})
     }
 	},
 }
