b2698b2d328d4e937e00654ebee31280343d78dd
Resoki <remyaubert71@gmail.com>
2022-03-24 16:47:51 +0100

    test

--- Evenements/joinevent.js
+++ Evenements/joinevent.js
@@ -10,12 +10,15 @@ module.exports = {
 
         //Si luser a encore le role après 10 min
         const test = () => {
-            if(member.roles.cache.has(global.roleKick))
-                member.kick().catch(()=>console.log(`Une erreur a eu lieu ${err}`))
-                console.log(`${member.user.username} a été kick au bout des 10 minutes!`)
+            if(member.roles.cache.has(global.roleKick)){
                 const channel = member.guild.channels.cache.find(channel => channel.id === global.channelJoin);
-
-                const successKick = new MessageEmbed().setDescription(`${member.user.username} a été kick`).setColor('GREEN');
+                member.kick().catch(()=>{
+                    const errorKick = new MessageEmbed().setDescription(`Une erreur a eu lieu en tentant de kick ${member.user.username} ! ❌`).setColor('RED');
+                    channel.send({embeds: [errorKick]})
+                })
+                console.log(`${member.user.username} a été kick au bout des 10 minutes! ✅`)
+               
+                const successKick = new MessageEmbed().setDescription(`${member.user.username} a été kick au bout des 10 minutes ! ✅`).setColor('GREEN');
                 channel.send({embeds: [successKick]})
               
             }
